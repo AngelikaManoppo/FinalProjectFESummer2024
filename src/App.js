3bcd47fa3544c9bd79a2995955029e3853c67257
angelikaMn <angelikamanoppo02@gmail.com>
2024-07-02 14:22:37 +0700

    updated with firebase config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Homepage from "./components/page/homepage";
 import "./assets/css/style.css";
+import "./config/firebase/index.js";
 
 const App = () => {
   return <Homepage />;
---
+++ src/config/firebase/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxtyfBMtt1FjeN0-VXNgrWCw4A_o4TfYg",
+  authDomain: "finalfesummer2024angel.firebaseapp.com",
+  databaseURL: "https://finalfesummer2024angel-default-rtdb.firebaseio.com",
+  projectId: "finalfesummer2024angel",
+  storageBucket: "finalfesummer2024angel.appspot.com",
+  messagingSenderId: "572008831212",
+  appId: "1:572008831212:web:d423024a087125d50dedc9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
